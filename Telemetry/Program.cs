98121ae06a3866209bf4a95cf25a576843fd9c5c
Guillermo Jackson <gijcancer@gmail.com>
2022-06-01 03:08:01 -0400

    Added functionality to code that allows it to resize when showing telemetry strips

--- Telemetry/Program.cs
+++ Telemetry/Program.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
+using System.Text;
+using System.IO;
 
 namespace Waves
 {
+
     public class Waves
     {
         public static void Main()
@@ -13,33 +17,39 @@ namespace Waves
             var begin = Console.ReadLine();
             Console.WriteLine(begin);
             if (begin == "yes")
+                
+                {
+                    int origWidth, width;
+                    int origHeight, height;
 
-            
+                    origWidth = Console.WindowWidth;
+                    origHeight = Console.WindowHeight;
+
+                    width = origWidth + 100;
+                    height = origHeight;
+                    Console.SetWindowSize(width, height);
+
+                }
+
+
             {
-                string NSRWave = @"
-
-
-                                               x
-                                              xxx
-                                              x x
-                                              x x
-                                             xx xx
-                                             x   x
-                                             x   x
-                             x              xx   xx            x
-                           xxxxx            x     x          xxxxx
-                          xx   xx           x     x         xx   xx
-                         xx     xx          x     x        xx     xx
-         xxxxxxxxxxxxxxxxx       xxxxx     x       x xxxxxxx       xxxxxxxx
-                                     x     x       x x
-                                     x     x       x x
-                                     xx   xx       xxx
-                                      x   x         x
-                                      x   x
-                                      xx xx
-                                       x x
-                                       xxx
-                                        x";
+                string NSRWave = @"                x                                 x                                 x                                 x                                 x                                 x
+               xx                                xx                                xx                                xx                                xx                                xx
+               xxx                               xxx                               xxx                               xxx                               xxx                               xxx
+               x x                               x x                               x x                               x x                               x x                               x x
+               x x                               x x                               x x                               x x                               x x                               x x
+              xx x         xx                   xx x         xx                   xx x         xx                   xx x         xx                   xx x         xx                   xx x         xx
+   xx         x  x        xxxx       xx         x  x        xxxx       xx         x  x        xxxx       xx         x  x        xxxx       xx         x  x        xxxx       xx         x  x        xxxx
+  xxxx        x  xx      xx  xx     xxxx        x  xx      xx  xx     xxxx        x  xx      xx  xx     xxxx        x  xx      xx  xx     xxxx        x  xx      xx  xx     xxxx        x  xx      xx  xx
+xxx  xxxxxx   x   x    xxxx   xxxxxxx  xxxxxx   x   x    xxxx   xxxxxxx  xxxxxx   x   x    xxxx   xxxxxxx  xxxxxx   x   x    xxxx   xxxxxxx  xxxxxx   x   x    xxxx   xxxxxxx  xxxxxx   x   x    xxxx   xxxx
+──────────xx──x───x────x────────────────────xx──x───x────x────────────────────xx──x───x────x────────────────────xx──x───x────x────────────────────xx──x───x────x────────────────────xx──x───x────x──────────
+           x  x   xx  x                      x  x   xx  x                      x  x   xx  x                      x  x   xx  x                      x  x   xx  x                      x  x   xx  x
+           xxx     x xx                      xxx     x xx                      xxx     x xx                      xxx     x xx                      xxx     x xx                      xxx     x xx
+            xx     x x                        xx     x x                        xx     x x                        xx     x x                        xx     x x                        xx     x x
+            x      xxx                        x      xxx                        x      xxx                        x      xxx                        x      xxx                        x      xxx
+                   xx                                xx                                xx                                xx                                xx                                xx
+                   xx                                xx                                xx                                xx                                xx                                xx
+                   x                                 x                                 x                                 x                                 x                                 x";
                 Console.WriteLine(NSRWave);
                 Console.ReadLine();
             }
