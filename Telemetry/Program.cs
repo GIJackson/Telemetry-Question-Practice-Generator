0ca80e2231bee48e0f3acb1a02d42af1e6171770
Guillermo Jackson <gijcancer@gmail.com>
2022-06-01 20:11:49 -0400

    New instructions with the ability to view a single telmetry strip from the List<Waves>. The user can now loop between '1' to answer the question, '2' to exit the command line, or any other key to be told 'That is not an option' and loop back to being asked to press 1 or 2. Still very clunky.

--- Telemetry/Program.cs
+++ Telemetry/Program.cs
@@ -2,58 +2,128 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System;
 using System.Text;
 using System.IO;
 
 namespace Waves
 {
-
-    public class Waves
+        public class Program
     {
         public static void Main()
         {
-            Console.WriteLine("Would you like to begin? Answer: Yes, No, Help, Quit");
-            var begin = Console.ReadLine();
-            Console.WriteLine(begin);
-            if (begin == "yes")
-                
+
+            Dictionary<string, string> waves = new()
+            {
+                { "Sinus Bradycardia", @"              x                                          x                                          x                                          x                                          x
+              xx                                         xx                                         xx                                         xx                                         xx
+              x x                                        x x                                        x x                                        x x                                        x x
+              x  x                                       x  x                                       x  x                                       x  x                                       x  x
+              x  x                                       x  x                                       x  x                                       x  x                                       x  x
+              x  x                                       x  x                                       x  x                                       x  x                                       x  x
+              x  x         xx                            x  x         xx                            x  x         xx                            x  x         xx                            x  x         xx
+   xx         x  x        x  x                xx         x  x        x  x                xx         x  x        x  x                xx         x  x        x  x                xx         x  x        x  x
+  x  x        x   x      x    x              x  x        x   x      x    x              x  x        x   x      x    x              x  x        x   x      x    x              x  x        x   x      x    x
+xx    xxxxx   x   x     x      xxxxxxxxxxxxxx    xxxxx   x   x     x      xxxxxxxxxxxxxx    xxxxx   x   x     x      xxxxxxxxxxxxxx    xxxxx   x   x     x      xxxxxxxxxxxxxx    xxxxx   x   x     x      xxxxxxxxxxxxx
+──────────xx──x───x────x─────────────────────────────xx──x───x────x─────────────────────────────xx──x───x────x─────────────────────────────xx──x───x────x─────────────────────────────xx──x───x────x────────────────────
+           x  x   xx  x                               x  x   xx  x                               x  x   xx  x                               x  x   xx  x                               x  x   xx  x
+           x x     x xx                               x x     x xx                               x x     x xx                               x x     x xx                               x x     x xx
+            xx     x x                                 xx     x x                                 xx     x x                                 xx     x x                                 xx     x x
+            x      x x                                 x      x x                                 x      x x                                 x      x x                                 x      x x
+                   xx                                         xx                                         xx                                         xx                                         xx
+                   xx                                         xx                                         xx                                         xx                                         xx
+                   x                                          x                                          x                                          x                                          x" },
+                { "Normal Sinus Rhythm", @"              x                                x                               x                               x                                x                                x                               x
+              xx                               xx                              xx                              xx                               xx                               xx                              xx
+              x x                              x x                             x x                             x x                              x x                              x x                             x x
+              x  x                             x  x                            x  x                            x  x                             x  x                             x  x                            x  x
+              x  x                             x  x                            x  x                            x  x                             x  x                             x  x                            x  x
+              x  x                             x  x                            x  x                            x  x                             x  x                             x  x                            x  x
+              x  x         xx                  x  x         xx                 x  x         xx                 x  x         xx                  x  x         xx                  x  x         xx                 x  x
+   xx         x  x        x  x      xx         x  x        x  x     xx         x  x        x  x     xx         x  x        x  x      xx         x  x        x  x      xx         x  x        x  x     xx         x  x
+  x  x        x   x      x    x    x  x        x   x      x    x   x  x        x   x      x    x   x  x        x   x      x    x    x  x        x   x      x    x    x  x        x   x      x    x   x  x        x   x
+xx    xxxxx   x   x     x      xxxx    xxxxx   x   x     x      xxx    xxxxx   x   x     x      xxx    xxxxx   x   x     x      xxxx    xxxxx   x   x     x      xxxx    xxxxx   x   x     x      xxx    xxxxx   x   x
+──────────xx──x───x────x───────────────────xx──x───x────x──────────────────xx──x───x────x──────────────────xx──x───x────x───────────────────xx──x───x────x───────────────────xx──x───x────x──────────────────xx──x───x──
+           x  x   xx  x                     x  x   xx  x                    x  x   xx  x                    x  x   xx  x                     x  x   xx  x                     x  x   xx  x                    x  x   xx
+           x x     x xx                     x x     x xx                    x x     x xx                    x x     x xx                     x x     x xx                     x x     x xx                    x x     x
+            xx     x x                       xx     x x                      xx     x x                      xx     x x                       xx     x x                       xx     x x                      xx     x
+            x      x x                       x      x x                      x      x x                      x      x x                       x      x x                       x      x x                      x      x
+                   xx                               xx                              xx                              xx                               xx                               xx                              xx
+                   xx                               xx                              xx                              xx                               xx                               xx                              xx
+                   x                                x                               x                               x                                x                                x                               x" },
+                { "Sinus Tachycardia", @"               x                     x                     x                     x                     x                     x                     x                     x                     x                     x
+               x                     x                     x                     x                     x                     x                     x                     x                     x                     x
+               x                     x                     x                     x                     x                     x                     x                     x                     x                     x
+              x x                   x x                   x x                   x x                   x x                   x x                   x x                   x x                   x x                   x x
+              x x                   x x                   x x                   x x                   x x                   x x                   x x                   x x                   x x                   x x
+              x x                   x x                   x x                   x x                   x x                   x x                   x x                   x x                   x x                   x x
+              x x                   x x                   x x                   x x                   x x                   x x                   x x                   x x                   x x                   x x
+  x     x     x  x      x     x     x  x      x     x     x  x      x     x     x  x      x     x     x  x      x     x     x  x      x     x     x  x      x     x     x  x      x     x     x  x      x     x     x  x
+ xxx   xxx    x  x     xxx   xxx    x  x     xxx   xxx    x  x     xxx   xxx    x  x     xxx   xxx    x  x     xxx   xxx    x  x     xxx   xxx    x  x     xxx   xxx    x  x     xxx   xxx    x  x     xxx   xxx    x  x
+ x x   x x    x  x     x x   x x    x  x     x x   x x    x  x     x x   x x    x  x     x x   x x    x  x     x x   x x    x  x     x x   x x    x  x     x x   x x    x  x     x x   x x    x  x     x x   x x    x  x
+x───xxx───x───x───x───x───xxx───x───x───x───x───xxx───x───x───x───x───xxx───x───x───x───x───xxx───x───x───x───x───xxx───x───x───x───x───xxx───x───x───x───x───xxx───x───x───x───x───xxx───x───x───x───x───xxx───x───x───
+     x    x   x   x   x    x    x   x   x   x    x    x   x   x   x    x    x   x   x   x    x    x   x   x   x    x    x   x   x   x    x    x   x   x   x    x    x   x   x   x    x    x   x   x   x    x    x   x
+          x   x   x   x         x   x   x   x         x   x   x   x         x   x   x   x         x   x   x   x         x   x   x   x         x   x   x   x         x   x   x   x         x   x   x   x         x   x
+           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x
+           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x
+           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x     x x           x x
+            x       x             x       x             x       x             x       x             x       x             x       x             x       x             x       x             x       x             x
+                    x             x       x                     x                     x                     x                     x                     x                     x                     x
+                    x                     x                     x                     x                     x                     x                     x                     x                     x" }
+            };
+
+
+
+            Console.WriteLine("Welcome to Telemetry Question Generator! Press enter to continue.");
+            Console.ReadLine();
+            Console.WriteLine("Would you like to begin your test? Enter 1 to begin, enter 2 for more options (it just makes you quit right now).");
+
+            int origWidth, width;
+            int origHeight, height;
+
+            origWidth = Console.WindowWidth;
+            origHeight = Console.WindowHeight;
+
+            width = origWidth + 100;
+            height = origHeight;
+
+            string begin;
+            while ((begin = Console.ReadLine()) != "2")
+            {
+                Console.WriteLine(begin);
+                if (begin == "1")
                 {
-                    int origWidth, width;
-                    int origHeight, height;
-
-                    origWidth = Console.WindowWidth;
-                    origHeight = Console.WindowHeight;
-
-                    width = origWidth + 100;
-                    height = origHeight;
                     Console.SetWindowSize(width, height);
 
+                    List<string> keyList = new(waves.Keys);
+                    Random rand = new();
+                    string randomKey = keyList[rand.Next(keyList.Count)];
+                    Console.WriteLine(waves[randomKey]);
+
+                    Console.WriteLine("What rhythm does this represent?");
+
+                    var answer = Console.ReadLine();
+                    if (answer == randomKey)
+                    {
+                        Console.WriteLine("That is correct! Please press 2 to exit");
+                    }
+                    else
+                    {
+                        Console.WriteLine("That was incorrect. Please press 1 to try again.");
+                    }
+
+                }
+                else if (begin == "2")
+                {
+                    Environment.Exit(0);
                 }
 
-
-            {
-                string NSRWave = @"                x                                 x                                 x                                 x                                 x                                 x
-               xx                                xx                                xx                                xx                                xx                                xx
-               xxx                               xxx                               xxx                               xxx                               xxx                               xxx
-               x x                               x x                               x x                               x x                               x x                               x x
-               x x                               x x                               x x                               x x                               x x                               x x
-              xx x         xx                   xx x         xx                   xx x         xx                   xx x         xx                   xx x         xx                   xx x         xx
-   xx         x  x        xxxx       xx         x  x        xxxx       xx         x  x        xxxx       xx         x  x        xxxx       xx         x  x        xxxx       xx         x  x        xxxx
-  xxxx        x  xx      xx  xx     xxxx        x  xx      xx  xx     xxxx        x  xx      xx  xx     xxxx        x  xx      xx  xx     xxxx        x  xx      xx  xx     xxxx        x  xx      xx  xx
-xxx  xxxxxx   x   x    xxxx   xxxxxxx  xxxxxx   x   x    xxxx   xxxxxxx  xxxxxx   x   x    xxxx   xxxxxxx  xxxxxx   x   x    xxxx   xxxxxxx  xxxxxx   x   x    xxxx   xxxxxxx  xxxxxx   x   x    xxxx   xxxx
-──────────xx──x───x────x────────────────────xx──x───x────x────────────────────xx──x───x────x────────────────────xx──x───x────x────────────────────xx──x───x────x────────────────────xx──x───x────x──────────
-           x  x   xx  x                      x  x   xx  x                      x  x   xx  x                      x  x   xx  x                      x  x   xx  x                      x  x   xx  x
-           xxx     x xx                      xxx     x xx                      xxx     x xx                      xxx     x xx                      xxx     x xx                      xxx     x xx
-            xx     x x                        xx     x x                        xx     x x                        xx     x x                        xx     x x                        xx     x x
-            x      xxx                        x      xxx                        x      xxx                        x      xxx                        x      xxx                        x      xxx
-                   xx                                xx                                xx                                xx                                xx                                xx
-                   xx                                xx                                xx                                xx                                xx                                xx
-                   x                                 x                                 x                                 x                                 x                                 x";
-                Console.WriteLine(NSRWave);
-                Console.ReadLine();
+                else
+                {
+                    Console.WriteLine("That is not an option, please press enter to continue");
+                    Console.ReadLine();
+                    Console.WriteLine("Would you like to begin your test? Enter 1 to begin, enter 2 for more options (it just makes you quit right now).");
+                }
             }
-
         }
     }
 }
