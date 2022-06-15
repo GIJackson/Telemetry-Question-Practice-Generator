c9c7e7dd42b55c16a0dc30d8f8b5630cd3aa4485
Guillermo Jackson <gijcancer@gmail.com>
2022-06-14 20:03:05 -0400

    Small commit post CodeKentucky Zoom Class
    Removed most of the green ramen errors Visual Studio 2022 was indicating
    regarding the possible null answers of userAnswer inputs as well as
    added a "?" after string for user input that was warning of possible
    null input. Added WIP section for catching errors associating with
    changing window size.

--- Telemetry/Program.cs
+++ Telemetry/Program.cs
@@ -68,25 +68,68 @@ x───xxx───x───x───x───x───xxx───x─�
             x       x             x       x             x       x             x       x             x       x             x       x             x       x             x       x             x       x             x
                     x             x       x                     x                     x                     x                     x                     x                     x                     x
                     x                     x                     x                     x                     x                     x                     x                     x                     x" },
-                {"a-fib", "a-fib"},
-                {"1st degree block", "1st degree block"},
-                {"2nd degree block", "2nd degree block"},
-                {"vtach", "vtach"},
-                {"asystole", "asystole"},
-                {"vfib", "vfib"}
+                { "Atrial Fibrillation", @"              x                                            x                                       x                  x                 x                                        x                                     x
+             x x                                          x x                                     x x                x x               x x                                      x x                                   x x
+             x x                                          x x                                     x x                x x               x x                                      x x                                   x x
+             x x                                          x x                                     x x                x x               x x                                      x x                                   x x
+             x x                                          x x                                     x x                x x               x x                                      x x                                   x x
+            x   x                                        x   x                                   x   x              x   x             x   x                                    x   x                                 x   x
+    x       x   x                                        x   x                     x             x   x              x   x             x   x                                    x   x                  x              x   x
+   xxx      x   x                  x          x          x   x          x         xxx            x   x              x   x             x   x           x          x             x   x               xxx x    xx       x   x
+  xx xx     x   x                 xxx        x x         x   x         x x       xx  x      x    x   x              x   x             x   x         xx xx    x x  x   x xx     x   x          x   x    x   x  x      x   x
+xxx   xxx   x   x        x   x   xx xx    xxx  x     x   x   x        x   x   xxxx     xx  xx  x x   x       x  x   x   x         x   x   x        x     xx x x     xx    x    x   x         x x x      xxx    xx    x   x
+─────────x x─────x──────x x x──x──────xxxx──────x───x x x─────x──────x──────x────────────xx x x┼x─────x─────x──x──xx─────x──────xx xxx─────x──────x────────x───────────────xxxx─────x───────x───x────────────────xxxx─────x──
+          x      x     x   x                     x x   x      x     x                        x        x    x             x     x           x     x                                  x     xx                              x
+                 x    x                           x           x    x                                  x    x             x    x            x    x                                   x    x                                x
+                  x   x                                        x   x                                   x   x              x   x             x   x                                    x   x                                 x
+                  x   x                                        x   x                                   x   x              x   x             x   x                                    x   x                                 x
+                  x   x                                        x   x                                   x   x              x   x             x   x                                    x   x                                 x
+                   x x                                          x x                                     x x                x x               x x                                      x x                                   x
+                    x                                            x                                       x                  x                 x                                        x
+                    x                                            x                                       x                  x                 x                                        x"},
+                { "1st degree block", "1st degree block"},
+                { "2nd degree block", "2nd degree block"},
+                { "vtach", "vtach"},
+                { "Asystole", @"
+
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+"},
+                { "Ventricular Fibrillation", @"                                                                            xxxxx
+                                                                           xx   xx                                                     xx         x xx
+                   xxxxx                                           xxx     x     x                        xxx             xxxx       x  xx       x   x                                             xxxxx
+                   x   xx                                         x  xx    x     x                       xx xxx          xx  x      x     x    xx    xx                            xx             xx   x
+         xxxxxx    x    x                   xxxxxxxx    xxxx xx  xx   xx   x     xxxxxxxxxx       xxxx   x    xxxxxxx  xx    x    x       x   xx      x    xxxxxxxx    xxxxx  xxx xx x       xxxxxx    xxxxxx         xxxxxxx
+       xxx    xx   x     xx xxxxxxxxxxx   xxx      xx  xx  xx xxx      x  xx              xxxxxxxxx   xx x           xxx     xxxxxx       xxxxx       xxxxxx      xxxxxx   xxxx  xx   xxxxxxxx              xxxxxxxxxxx     x
+ xxxxxx        xxxxx      xxx         xxxx          xxxx               xxxx                            xx
+─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+"}
             };
 
-            while (true)
+            Console.WriteLine("Welcome to Telemetry Question Generator! \n\nPress any key to continue.");
+            Console.ReadKey();
+            Console.WriteLine();
+            bool continueQuiz = true;
+            while (continueQuiz == true)
             {
-                Console.WriteLine("Welcome to Telemetry Question Generator! \n\nPress any key to continue.");
-                Console.ReadKey();
-                Console.WriteLine();
                 Console.WriteLine("Would you like to begin your test? \n\nEnter 1 to begin, enter 2 to quit.");
-                string begin = Console.ReadLine();
+                string? begin = Console.ReadLine();
 
                 if (begin == "1")
                 {
-                    Console.SetWindowSize(220, 40);
+                    try
+                    {
+                        Console.SetWindowSize(220, 40);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        //create error log here-ish
+                        Console.WriteLine("It appears your monitor is not ultrawide.");
+                        throw;
+                    }
+                    
 
                     List<string> randomKey = new(waves.Keys);
 
@@ -95,16 +138,23 @@ x───xxx───x───x───x───x───xxx───x─�
                     var questionKey = randomKeyList.ToArray();
                     for (int i = 0; i < questionKey.Length; i++)
                     {
+                        //var questionKeyLength = questionKey.Length - 1;
                         Console.WriteLine(waves[questionKey[i]]);
                         Console.WriteLine("What rhythm does this represent?");
                         var answer = questionKey[i];
                         var userAnswer = Console.ReadLine();
-                        if(i == (questionKey.Length - 1) && userAnswer.ToUpper() == answer.ToUpper())
+                        if (String.IsNullOrEmpty(userAnswer))
+                        {
+                            Console.WriteLine("How did you do that?");
+                            break;
+                        }
+                        else if (i == (questionKey.Length - 1) && userAnswer.ToUpper() == answer.ToUpper())
                         {
                             Console.WriteLine("Correct!\n\nYou have completed the last question, congratulations!\n\n");
                             Console.WriteLine("Enter any key to return to the main menu.");
                             break;
                         }
+                        
                         else if (i == (questionKey.Length - 1) && userAnswer.ToUpper() != answer.ToUpper())
                         {
                             Console.WriteLine("That was not correct.\n\nYou have completed the last question, congratulations!\n\n");
