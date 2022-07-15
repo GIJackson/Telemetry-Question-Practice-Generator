428c36886e8b57fc28f05a5d969e45cdfdd867e5
Guillermo Jackson <gijcancer@gmail.com>
2022-07-14 21:53:16 -0400

    Class CSVWriterNew and CSVSaveReader
    Began work on creating classes that read and write to .csv files. CSVWriterNew can create a .csv files and append but not overwrite if one already exists. At this time, CSVSaveReader can read a .csv file and return a list of selected strings in an array of lines read from the file.

---
+++ Telemetry/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telemetry
+{
+    public class User
+    {
+        public int _ID;
+        public string _firstName, _lastName;
+        public string _name;
+
+        public void CSVWriterNew(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter sw = new(filePath, true))
+                {
+                    sw.WriteLine("ID" + "," + "First Name" + "," + "Last Name");
+                }
+            }
+            using (StreamWriter sw = new(filePath, true))
+            {
+                sw.WriteLine(_ID + "," + _firstName + "," + _lastName);
+
+            }
+        }
+        public List<string> CSVSaveReader(List<string> userData, int rowDataPosition, string filePath)
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                userData = new List<string>();
+                string? line = sr.ReadLine();
+                while (line != null)
+                {
+                    string[] lineArray = line.Split(',');
+                    for (int i = 0; i < line.Split(',').Count(); i++)
+                    {
+                        userData.Add(lineArray[rowDataPosition]);
+                    }
+                }
+                return userData;
+            }
+        }
+    }
+}
